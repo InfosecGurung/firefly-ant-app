f09ccce1b6a27d39cbaa247bc82e8132bee29fbe
Infosec Gurung <72032538+infosecgurung@users.noreply.github.com>
2021-08-03 22:48:14 +0530

    The app shows name of person who has no access
    Signed-off-by: InfosecGurung <72032538+InfosecGurung@users.noreply.github.com>

--- app/src/main/java/com/example/fireflyant/DisplayWrongMessage.kt
+++ app/src/main/java/com/example/fireflyant/DisplayWrongMessage.kt
@@ -5,17 +5,17 @@ import android.os.Bundle
 import android.widget.TextView
 
 class DisplayWrongMessage : AppCompatActivity() {
-    private val wrong = "YOU ARE NOT ANTS"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_wrong_message)
 
         /* Get the Intent that started this activity and extract the string */
         val message = intent.getStringExtra(EXTRA_MESSAGE)
-
+        val wrong = "hi "
+        val fullMessage = " , you don't have access!"
         val textView = findViewById<TextView>(R.id.wrongTextView).apply {
-            text = wrong
+            val welcomeWrongName = wrong + message + fullMessage
+            text = welcomeWrongName
         }
     }
 }
